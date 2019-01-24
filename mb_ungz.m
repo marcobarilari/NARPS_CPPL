7df30b1867f93db19d10d06ce7ef29c68c97c8c3
barilari <barilari@mac-554-749.local>
2019-01-24 15:30:16 +0100

    Correct typos, vars style and other minors

--- mb_ungz.m
+++ mb_ungz.m
@@ -10,48 +10,49 @@ folder_main = dir(folder_path);
 % Remove . and ..
 folder_main(ismember( {folder_main.name}, {'.', '..'})) = [];
 % Get a logical vector that tells which is a directory.
-dirFlags = [folder_main.isdir];
+dir_flags = [folder_main.isdir];
 % Extract only those that are directories.
 folder_subj = folder_main(dirFlags);
 % Extract only those that are subj folders.
 folder_subj(~strncmp( {folder_subj.name}, {'sub'}, 3)) = [];
-% Loop across floder and unpack .gz files
+% Loop across folder and unpack .gz files
 for k = 1 : length(folder_subj)
 	fprintf('\nSub folder #%d = %s\n', k, folder_subj(k).name);
 	% Anatomical data
-	fprintf('\n Unpacoctaveking anat\n');
+	fprintf('\n Unpaking anat\n');
 	% Build subj folder path
-	temp = fullfile(folder_path, folder_subj(k).name, '/anat');
+	temp = fullfile(folder_path, folder_subj(k).name, 'anat');
 	% Check if there are .gz file, then unpack them
 	if size(dir(fullfile(temp,'*.gz')),1)
 		% Make a list of the file in it with '.gz' extension
-		files = ls(fullfile(temp, '*.gz'));
+		file_list = ls(fullfile(temp, '*.gz'));
 		% Unzip the '.gz' files
-		for ifile = 1:size(files,1)
-			fprintf('\n  Unpacking file #%d of %d\n', ifile, size(files,1));
+		for ifile = 1:size(file_list,1)
+			fprintf('\n  Unpacking file #%d of %d\n', ifile, size(file_list,1));
 			% Print the file name
-			fName = strsplit(files(ifile,:), filesep);
-			fprintf('  %s\n', cell2mat(fName(size(fName,2))));
-			gunzip(files(ifile,:));
+			file_name = strsplit(file_list(ifile,:), filesep);
+			fprintf('  %s\n', cell2mat(file_name(size(file_name,2))));
+			gunzip(file_list(ifile,:));
 		end
-	else disp('no files to unpack') end
+	else disp('no files to unpack')end
 	% Functional data
 	fprintf('\n Unpacking func\n');
 	% Build subj folder path
-	temp = fullfile(folder_path, folder_subj(k).name, '/func');
+	temp = fullfile(folder_path, folder_subj(k).name, 'func');
 	% Check if there are .gz file, then unpack them
 	if size(dir(fullfile(temp,'*.gz')),1)
 		% Make a list of the file in it with '.gz' extension
-		files = ls(fullfile(temp, '*.gz'));
-		for ifile = 1:size(files,1)
-			fprintf('\n  Unpacking file #%d of %d\n', ifile, size(files,1));
+		file_list = ls(fullfile(temp, '*.gz'));
+		for ifile = 1:size(file_list,1)
+			fprintf('\n  Unpacking file #%d of %d\n', ifile, size(file_list,1));
 			% Print the file name
-			fName = strsplit(files(ifile,:), filesep);
-			fprintf('  %s\n', cell2mat(fName(size(fName,2))));
+			file_name = strsplit(file_list(ifile,:), filesep);
+			fprintf('  %s\n', cell2mat(file_name(size(file_name,2))));
 			% Unzip the '.gz' files
-			gunzip(files(ifile,:));
+			gunzip(file_list(ifile,:));
 		end
-	else disp('no files to unpack') end
+	else disp('no files to unpack')end
 end
 
 end
+
