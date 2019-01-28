4bd803389ccc99e68b5109ff12c93c102275b28c
Remi Gau <remi_gau@hotmail.com>
2019-01-28 11:28:52 +0100

    add filter for spm_select to only unzip certain files

--- subfun/unzip_fmriprep.m
+++ subfun/unzip_fmriprep.m
@@ -1,4 +1,4 @@
-function unzip_fmriprep(folder_path)
+function unzip_fmriprep(folder_path, filter)
 
 % Function to unzip '.gz' files, tested on octave
 % written by mb, 23/01/2019
@@ -15,6 +15,13 @@ if nargin == 0
 elseif ~exist(folder_path, 'dir')
     error('The path is not a folder, please insert a valid folder path')
 end
+
+% if no filter for spm_select is given we will select and unzip all .nii.gz
+% files
+if nargin < 2
+    filter = '^*.nii.gz$';
+end
+
 % Get a list of all files and folders in this folder.
 folder_main = dir(folder_path);
 % Remove . and ..
@@ -40,7 +47,7 @@ for k = 1 : length(folder_subj)
     % Build subj folder path
     folder_files = fullfile(folder_path, folder_subj(k).name, 'func');
     % Check if there are .gz file, then unpack them
-    unpack_gz(folder_files)
+    unpack_gz(folder_files, filter)
 end
 
 toc
@@ -48,19 +55,24 @@ toc
 end
 
 
-function unpack_gz(folder_files)
-if size(dir(fullfile(folder_files,'*.gz')),1)
-    % Make a list of the file in it with '.gz' extension
-    file_list = spm_select('FPList', folder_files, '^*.nii.gz$');
+function unpack_gz(folder_files, filter)
+
+% Make a list of the file in it with '.gz' extension
+file_list = spm_select('FPList', folder_files, filter);
+
+if ~isempty(file_list)
     % Unzip the '.gz' files
     for ifile = 1:size(file_list,1)
-        fprintf('\n  Unpacking file #%d of %d\n', ifile, size(file_list,1));
+        
         % Print the file name
+        fprintf('\n  Unpacking file #%d of %d\n', ifile, size(file_list,1));
         file_name = strsplit(file_list(ifile,:), filesep);
         fprintf('  %s\n', cell2mat(file_name(size(file_name,2))));
+        
         gunzip(file_list(ifile,:));
     end
 else
     fprintf('\nno ".gz" files to unpack\n')
 end
+
 end
