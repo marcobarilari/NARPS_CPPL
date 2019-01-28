d2ce2bdd1605d6bbc76f2dd0061773daa0ecf53d
Remi Gau <remi_gau@hotmail.com>
2019-01-28 13:57:52 +0100

    remove old unzipping function

--- subfun/unzip_file.m
+++
@@ -1,8 +0,0 @@
-function filename = unzip_file(filename)
-
-[filepath, name, ext] = spm_fileparts(filename);
-if strcmp(ext, '.gz')
-    gunzip(filename)
-    filename = fullfile(filepath, name);
-end
-end
