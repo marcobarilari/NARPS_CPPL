864168913d401ba28f0a4afd54dff7f828434937
Remi Gau <remi_gau@hotmail.com>
2019-01-28 11:01:12 +0100

    change function name and fix typo

--- subfun/unzip.m
+++ subfun/unzip.m
@@ -1,4 +1,4 @@
-function mb_ungz(folder_path)
+function unzip(folder_path)
 
 % Function to unzip '.gz' files, tested on octave
 % written by mb, 23/01/2019
@@ -25,11 +25,12 @@ dir_flags = [folder_main.isdir];
 folder_subj = folder_main(dir_flags);
 % Extract only those that are subj folders.
 folder_subj(~strncmp( {folder_subj.name}, {'sub'}, 3)) = [];
+
 % Loop across folder and unpack .gz files
 parfor k = 1 : length(folder_subj)
     fprintf('\nSub folder #%d = %s\n', k, folder_subj(k).name);
     % Anatomical data
-    fprintf('\n Unpaking anat\n');
+    fprintf('\n Unpacking anat\n');
     % Build subj folder path
     folder_files = fullfile(folder_path, folder_subj(k).name, 'anat');
     % Check if there are .gz file, then unpack them
@@ -42,23 +43,24 @@ parfor k = 1 : length(folder_subj)
     unpack_gz(folder_files)
 end
 
-function unpack_gz(folder_files)
-if size(dir(fullfile(folder_files,'*.gz')),1)
-        % Make a list of the file in it with '.gz' extension
-        file_list = ls(fullfile(folder_files, '*.gz'));
-        % Unzip the '.gz' files
-        parfor ifile = 1:size(file_list,1)
-            fprintf('\n  Unpacking file #%d of %d\n', ifile, size(file_list,1));
-            % Print the file name
-            file_name = strsplit(file_list(ifile,:), filesep);
-            fprintf('  %s\n', cell2mat(file_name(size(file_name,2))));
-            gunzip(file_list(ifile,:));
-        end
-    else
-        fprintf('\nno ".gz" files to unpack\n')
-    end
-end
-
 toc
 
+end
+
+
+function unpack_gz(folder_files)
+if size(dir(fullfile(folder_files,'*.gz')),1)
+    % Make a list of the file in it with '.gz' extension
+    file_list = ls(fullfile(folder_files, '*.gz'));
+    % Unzip the '.gz' files
+    parfor ifile = 1:size(file_list,1)
+        fprintf('\n  Unpacking file #%d of %d\n', ifile, size(file_list,1));
+        % Print the file name
+        file_name = strsplit(file_list(ifile,:), filesep);
+        fprintf('  %s\n', cell2mat(file_name(size(file_name,2))));
+        gunzip(file_list(ifile,:));
+    end
+else
+    fprintf('\nno ".gz" files to unpack\n')
+end
 end
