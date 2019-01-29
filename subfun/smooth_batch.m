90c60a37ee9f0ddacb288366df4da2871ec1abcb
Remi Gau <remi_gau@hotmail.com>
2019-01-29 11:03:41 +0100

    smooth_batch.m change filename, add filter as argument, silence mlint warnings

--- subfun/smooth_batch.m
+++ subfun/smooth_batch.m
@@ -1,6 +1,4 @@
-function mb_smooth(FWHM, prefix, folder_path)
-
-% spm fmri
+function smooth_batch(FWHM, prefix, folder_path, filter)
 
 tic
 
@@ -11,12 +9,14 @@ if ~exist('FWHM', 'var')
 else
     fprintf('\n- FWHM set to %d mm\n', FWHM)
 end
+
 % Check about 'prefix' var input
 if ~exist('prefix', 'var')
     fprintf('\n- "prefix" not provided, set to "s" as default\n')
     prefix = 's';
-else fprintf('\n- "prefix" of the smoothed data is %s\n', prefix);
+else fprintf('\n- "prefix" of the smoothed data is %s\n', prefix); %#ok<*SEPEX>
 end
+
 % Check about 'folder_path' var input
 if ~exist('folder_path', 'var')
     folder_path = pwd;
@@ -26,14 +26,19 @@ elseif ~exist(folder_path, 'dir')
 else fprintf('\n- I will look for data to smooth here %s\n', folder_path);
 end
 
-% get subject list and folders
+% Filter only the func data to smooth
+if ~exist('filter', 'var')
+    filter =  'sub-.*space-MNI152.*preproc.nii$';
+    fprintf('\n- filter not provided, set to %s as default\n', filter)
+else fprintf('\n- "prefix" of the smoothed data is %s\n', filter);
+end
+
+
+% Get subject list and folders
 folder_subj = get_subj_list(folder_path);
 
-% Filter only the func data to smooth
-filter =  'sub-.*space-MNI152.*preproc.nii$';
-
 % Loop across folder and unpack .gz files
-for isubj = 1 : length(folder_subj)
+parfor isubj = 1 : length(folder_subj)
     
     matlabbatch = [];
     
@@ -42,8 +47,6 @@ for isubj = 1 : length(folder_subj)
     folder_files = fullfile(folder_path, folder_subj(isubj).name, 'func');
     % Make a list of the file in it with '.gz' extension
     file_list = cellstr(spm_select('ExtFPList', folder_files, filter, Inf));
-    
-    
     
     % Create the batch
     matlabbatch{1}.spm.spatial.smooth.data   = cellstr(file_list);
@@ -62,7 +65,7 @@ toc
 end
 
 
-function save_the_job(matlabbatch, folder_files, folder_subj_name)
+function save_the_job(matlabbatch, folder_files, folder_subj_name) %#ok<INUSL>
 job_name = fullfile(folder_files,['job_' folder_subj_name '_smoothing.mat' ]);
 save(job_name, 'matlabbatch')
 end
