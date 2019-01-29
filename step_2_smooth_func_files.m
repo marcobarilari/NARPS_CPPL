a4a086c4c2cf35c5aee99133afdce75d24d33f4c
Remi Gau <remi_gau@hotmail.com>
2019-01-29 12:05:59 +0100

    remove file transfer and add call to smoothing batch function

--- step_2_smooth_func_files.m
+++ step_2_smooth_func_files.m
@@ -7,33 +7,16 @@
 % -v /c/Users/Remi/Documents/NARPS/derivatives/:/output \
 % spmcentral/spm:octave-latest script '/code/step_2_smooth_func_files.m'
 
+clear 
+clc
+
+machine_id = 1;% 0: container ;  1: Remi ;  2: Marco
 FWHM = 6;
 prefix = 's';
-machine_id = 1;% 0: container ;  1: Remi ;  2: Marco
 filter =  'sub-.*space-MNI152NLin2009cAsym_preproc.nii'; % to smooth only the preprocessed files
 
 % setting up directories
 [data_dir, code_dir, output_dir, fMRIprep_DIR] = set_dir(machine_id);
 
-% creating output folders in derivatives/spm12
-mkdir(output_dir)
-folder_subj = get_subj_list(fMRIprep_DIR);
-folder_subj = cellstr(char({folder_subj.name}')); % turn subject folders into a cellstr
-spm_mkdir(output_dir, folder_subj, {'anat','func'});
-
 % smooth
-% INSERT CALL TO SMOOTHING FUNCTION
-
-% move smoothed file to output dir
-for i_subj = 1:size(folder_subj, 1)
-    
-    files_to_move = spm_select('FPList', ...
-        fullfile(fMRIprep_DIR, folder_subj{i_subj}, 'func'), ...
-        ['^' prefix '.*' filter '$'] ); 
-    
-    for i_file = 1:size(files_to_move,1)
-        movefile(files_to_move(i_file,:), ...
-            fullfile(output_dir), folder_subj{i_subj}, 'func')
-    end
-    
-end
+smooth_batch(FWHM, prefix, output_dir, filter)
