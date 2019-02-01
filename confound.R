ffa0f58c3263342cf351fa2b51b2d503e95a0be9
Remi Gau <remi_gau@hotmail.com>
2019-02-01 18:21:19 +0100

    test for PR

--- confound.R
+++ confound.R
@@ -1,4 +1,4 @@
-
+# this is a test
 
 files <- list.files(path=getwd(), pattern="*.tsv")
 
@@ -7,7 +7,7 @@ nsubj <- length(files)/4
 all.confound <- data.frame()
 
 for(file in files){
-  temp <- read.table(file, header = T, na.strings = 'n/a') 
+  temp <- read.table(file, header = T, na.strings = 'n/a')
   temp[is.na(temp)] <- 0
   temp <- temp$FramewiseDisplacement
   temp2 <- as.data.frame(temp)
@@ -26,7 +26,7 @@ threshold <- seq(from = 0.2, to = 1, by = 0.1)
 
 for (t in 1:length(threshold)){
   table.threshold <- data.frame()
-  
+
   for (sub in 1:length(levels(as.factor(all.confound$subjID)))){
     temp_subj <- subset(subset(all.confound, subjID==subj_list[sub]))
     for (run in 1:length(levels(as.factor(all.confound$runID)))){
@@ -36,13 +36,13 @@ for (t in 1:length(threshold)){
       table.threshold[sub,run+1] <- sum(temp_run$above_threshold)/nrow(temp_run)
     }
   }
-  
+
   table.threshold.2 <- table.threshold
   table.threshold[6:9] <- ifelse(table.threshold[2:5] > .1, 1, 0)
   table.threshold$drop <- ifelse(rowSums(table.threshold[6:9], dims = 1) > 0, 1, 0)
-  
+
   print(paste('At', threshold[t], 'mm threshold you discard n.', sum(table.threshold$drop), 'subj'))
-  
+
   # drop.list <- table.threshold[which(table.threshold$drop==1),1]
   # for (i in 1:length(drop.list)){
   # print(drop.list[i])
@@ -61,9 +61,9 @@ ggplot(all.confound.drop, aes(runID, FramewiseDisplacement, fill=runID))+
 
 # plot <- list()
 # subj_list <- levels(as.factor(all.confound$subjID))
-# 
+#
 # library(ggplot2)
-# 
+#
 # for (sub in 1:length(levels(as.factor(all.confound$subjID)))){
 #   sub_temp <- subset(all.confound,subjID==subj_list[sub])
 #   plot[[sub]] <- ggplot(sub_temp, aes(subjID, FramewiseDisplacement, fill=runID))+
@@ -71,9 +71,8 @@ ggplot(all.confound.drop, aes(runID, FramewiseDisplacement, fill=runID))+
 #     theme_bw()
 #   ggsave(plot = plot[[sub]], file = paste(subj_list[sub],".jpg",sep=""))
 # }
-# 
+#
 # ggplot(all.confound.drop, aes(runID, FramewiseDisplacement, fill=runID))+
 #   # geom_boxplot(show.legend = F)+
 #   geom_jitter(aes(col=subjID), show.legend = F)+
 #   theme_classic()
-
