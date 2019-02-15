8f65f36204ef152d2fd5611649df6d6f82a9f361
Remi Gau <remi_gau@hotmail.com>
2019-02-15 12:39:33 +0100

    remove commnents

--- subfun/get_cdt_onsets.m
+++ subfun/get_cdt_onsets.m
@@ -1,9 +1,6 @@
 function [onsets] = get_cdt_onsets(onsets, iRun)
 % Defines the different conditions we will use in the GLMs with their onset and durations 
 
-% TO DO
-% - separate missed trials from non-missed trials
-
 % identify missed responses
 onsets{iRun}.is_missed = strcmp(onsets{iRun}.participant_response, 'NoResp');
 
